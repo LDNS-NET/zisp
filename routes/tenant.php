212a720243e586a628245ef0b2f8e511bc5dafc2
LDNS NETWORKS <ldnsnetworks@gmail.com>
2025-11-16 22:15:00 +0300

    Initial commit

--- routes/tenant.php
+++ routes/tenant.php
@@ -25,7 +25,7 @@ Route::middleware([
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
     Route::get('/', function () {
-        return redirect('/login');
+        return redirect()->route('tenants.hotspot.show');
     });
 
     // Tenant hotspot landing page (external URL used by MikroTik hotspot login-page)
