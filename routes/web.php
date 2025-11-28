3388d91f38c9eefb0ab5f44263d90c2bda6c33a4
LDNS NETWORKS <ldnsnetworks@gmail.com>
2025-11-28 20:54:16 +0300

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['tenant.domain', 'check.subscription'])->group(function () {
+Route::middleware(['tenant.domain'])->group(function () {
 Route::resource('hotspot', TenantHotspotController::class);
 });
 
