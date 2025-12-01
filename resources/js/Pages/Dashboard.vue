c1ab382203ac456f9a460763b74caa86a318f2f9
LDNS NETWORKS <ldnsnetworks@gmail.com>
2025-12-02 01:47:48 +0300

    Initial commit

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -40,7 +40,8 @@ const getGreeting = () => {
     const hour = new Date().getHours();
     if (hour < 12) return 'Good morning';
     if (hour < 18) return 'Good afternoon';
-    return 'Good evening';
+    if (hour < 24) return 'Good evening';
+    return 'Good night';
 };
 
 const greeting = ref(getGreeting());
