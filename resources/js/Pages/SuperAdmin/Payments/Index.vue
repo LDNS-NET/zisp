465d09ca7e982287e663b9f21f3e835f9f10160d
LDNS NETWORKS <ldnsnetworks@gmail.com>
2025-12-05 22:19:13 +0300

    SuperAdmin

--- resources/js/Pages/SuperAdmin/Payments/Index.vue
+++ resources/js/Pages/SuperAdmin/Payments/Index.vue
@@ -20,7 +20,7 @@ const props = defineProps({
             <h2 class="text-2xl font-semibold leading-tight">
                 Payments
             </h2>
-        </template>>
+        </template>
         <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
