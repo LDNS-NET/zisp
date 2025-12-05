be89769ed29fff4fe08f36a215844c57912dbb9d
LDNS NETWORKS <ldnsnetworks@gmail.com>
2025-12-05 21:31:25 +0300

    SuperAdmin

--- app/Http/Controllers/SuperAdmin/UsersController.php
+++ app/Http/Controllers/SuperAdmin/UsersController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers\SuperAdmin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Tenants\NetworkUser;
+use App\Models\User;
+use App\Models\Tenants\TenantPayment;
+use App\Models\Tenant;
+use App\Models\Tenants\TenantGenenralSetting;
+use App\Models\Tenants\TenantMikrotik;
+
+
 
 class UsersController extends Controller
 {
-    //
+    public function index() {
+        $users = User::orderBy('created_at', 'desc')->paginate(20);
+
+        return Inertia::render('SuperAdmin/Users/Index', [
+            'users' => $users,
+        ]);
+    }
 }
