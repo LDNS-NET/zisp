5c41b4f66f760451d1087dddd6ec2f049144f8b2
LDNS NETWORKS <ldnsnetworks@gmail.com>
2025-12-05 23:21:50 +0300

    SuperAdmin

--- app/Http/Controllers/SuperAdmin/UsersController.php
+++ app/Http/Controllers/SuperAdmin/UsersController.php
@@ -9,7 +9,7 @@ use App\Models\Tenants\NetworkUser;
 use App\Models\User;
 use App\Models\Tenants\TenantPayment;
 use App\Models\Tenant;
-use App\Models\TenantGenenralSetting;
+use App\Models\TenantGeneralSetting;
 use App\Models\Tenants\TenantMikrotik;
 
 
@@ -33,7 +33,7 @@ class UsersController extends Controller
         // Fetch detailed tenant info from the all_tenants table (SuperAdmin\Users model)
         $tenantDetails = \App\Models\SuperAdmin\Users::where('tenant_id', $user->tenant_id)->first();
 
-        $tenantSettings = TenantGenenralSetting::where('tenant_id', $user->tenant_id)->first();
+        $tenantSettings = TenantGeneralSetting::where('tenant_id', $user->tenant_id)->first();
         $mikrotiks = TenantMikrotik::where('tenant_id', $user->tenant_id)->get();
         $totalEndUsers = NetworkUser::where('tenant_id', $user->tenant_id)->count();
         $totalPayments = TenantPayment::where('tenant_id', $user->tenant_id)->sum('amount');
