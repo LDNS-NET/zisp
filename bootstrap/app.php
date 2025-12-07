e60fe9bc4a89d99ef16a4a423c6103b5f92fd73b
LDNS NETWORKS <ldnsnetworks@gmail.com>
2025-12-07 15:41:11 +0300

    SuperAdmin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,10 +22,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'mikrotiks/*/sync',
             'mikrotiks/*/register-wireguard',
         ]);
-
-        $middleware->alias([
-            'check.subscription' => \App\Http\Middleware\CheckSubscription::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->renderable(function (\Stancl\Tenancy\Exceptions\TenantCouldNotBeIdentifiedException $e, \Illuminate\Http\Request $request) {
