9dc43ea308b83d2a0e083e39726ac25e7377e954
LDNS NETWORKS <ldnsnetworks@gmail.com>
2025-12-12 18:31:33 +0300

    Initial commit

--- resources/js/Pages/Mikrotiks/Show.vue
+++ resources/js/Pages/Mikrotiks/Show.vue
@@ -135,25 +135,34 @@ const updateIdentity = () => {
                             </SecondaryButton>
                         </template>
                         <template #content>
-                            <DropdownLink as="button" @click="refreshData">
+                            <button
+                                @click="refreshData"
+                                class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 transition duration-150 ease-in-out hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800"
+                            >
                                 <div class="flex items-center">
                                     <RefreshCw class="mr-2 h-4 w-4" :class="{ 'animate-spin': isRefreshing }" />
                                     Refresh Data
                                 </div>
-                            </DropdownLink>
-                            <DropdownLink as="button" @click="openIdentityModal">
+                            </button>
+                            <button
+                                @click="openIdentityModal"
+                                class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 transition duration-150 ease-in-out hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800"
+                            >
                                 <div class="flex items-center">
                                     <Edit class="mr-2 h-4 w-4" />
                                     Change Identity
                                 </div>
-                            </DropdownLink>
+                            </button>
                             <div class="border-t border-gray-100 dark:border-gray-700" />
-                            <DropdownLink as="button" @click="rebootRouter" class="text-red-600 dark:text-red-400">
+                            <button
+                                @click="rebootRouter"
+                                class="block w-full px-4 py-2 text-start text-sm leading-5 text-red-600 dark:text-red-400 transition duration-150 ease-in-out hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800"
+                            >
                                 <div class="flex items-center">
                                     <Power class="mr-2 h-4 w-4" />
                                     Reboot Router
                                 </div>
-                            </DropdownLink>
+                            </button>
                         </template>
                     </Dropdown>
                 </div>
