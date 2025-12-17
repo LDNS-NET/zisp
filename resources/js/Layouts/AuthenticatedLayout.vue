96c3c57b1de1bdc4918cedac02e0ec3b1d69c794
LDNS NETWORKS <ldnsnetworks@gmail.com>
2025-12-17 11:56:03 +0300

    Initial commit

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -119,6 +119,14 @@ function toggleSidebar() {
                         ZiSP
                     </span>
                 </div>
+                <div class="p-4 border-t border-gray-100 dark:border-slate-800 hidden lg:flex justify-end">
+                <button 
+                    @click="collapsed = !collapsed"
+                    class="p-2 rounded-lg text-gray-400 hover:bg-gray-100 hover:text-gray-600 dark:hover:bg-slate-800 dark:hover:text-gray-300 transition-colors"
+                >
+                    <component :is="collapsed ? ChevronLeft : ChevronRight" class="w-5 h-5 transform rotate-180" />
+                </button>
+            </div>
                 <!-- Mobile Close Button -->
                 <button @click="sidebarOpen = false" class="lg:hidden text-gray-500 hover:text-gray-700 dark:text-gray-400">
                     <X class="w-6 h-6" />
