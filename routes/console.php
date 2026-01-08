f93ff4caa6b01fc0e4fb1382397127fab04a1686
LDNS NETWORKS <ldnsnetworks@gmail.com>
2026-01-08 11:27:39 +0300

    Initial commit

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,29 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Scheduled Tasks (Laravel 12 format)
+Schedule::command('sms:send-expiry-notifications')->everyFiveMinutes();
+
+Schedule::command('routers:sync')
+    ->everyFourMinutes()
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('wireguard:sync-peers')
+    ->everyMinute()
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('users:disconnect-expired')->everyMinute();
+
+Schedule::command('app:cleanup-stale-sessions')->everyFiveMinutes();
+
+Schedule::command('network:process-upgrades')
+    ->everyMinute()
+    ->withoutOverlapping();
