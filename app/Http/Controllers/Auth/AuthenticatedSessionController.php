43dc8808c87164483b1875364a9e2cf5dac9bec5
LDNS NETWORKS <ldnsnetworks@gmail.com>
2026-01-22 19:56:22 +0300

    Initial commit

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -47,8 +47,7 @@ class AuthenticatedSessionController extends Controller
                 if ($setting && $setting->logo) {
                     // Convert relative path to full URL
                     if (!str_starts_with($setting->logo, 'http')) {
-                        $tenantLogo = asset('storage/' . $setting->logo);
-                        //$tenantLogo = storage::disk('public')->url($setting->logo);
+                        $tenantLogo = Storage::disk('public')->url($setting->logo);
                     } else {
                         $tenantLogo = $setting->logo;
                     }
