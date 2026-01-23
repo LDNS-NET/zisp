ba200f3ef01b2e162cc9ef79093e2770eb3383fc
LDNS NETWORKS <ldnsnetworks@gmail.com>
2026-01-23 10:15:33 +0300

    Initial commit

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -80,7 +80,7 @@ const navigation = [
 
     { header: 'Field Operations', roles: ['tenant_admin', 'admin', 'network_engineer', 'technical', 'technician'] },
     { name: 'My Installations', href: route('tenant.installations.my-installations'), icon: Layers, active: 'tenant.installations.my-installations', roles: ['technical', 'technician'], permission: 'view_installations' },
-    { name: 'All Installations', href: route('tenant.installations.index'), icon: Layers, active: 'tenant.installations.index', roles: ['tenant_admin', 'admin', 'network_engineer'], permission: 'view_installations' },
+    { name: 'All Installations', href: route('tenant.installations.index'), icon: Layers, active: 'tenant.installations.index', roles: ['tenant_admin', 'admin', 'network_engineer', 'technical', 'technician'], permission: 'view_installations' },
 
     { header: 'System & Security', roles: ['tenant_admin', 'network_admin'] },
     { name: 'Staff Management', href: route('settings.staff.index'), icon: UserCog, active: 'settings.staff.*', roles: ['tenant_admin'], permission: 'manage_staff' },
