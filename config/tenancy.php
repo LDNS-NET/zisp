2bc817165a864801acd5e1cc7d270ba8021218e5
LDNS NETWORKS <ldnsnetworks@gmail.com>
2026-02-25 21:54:11 +0300

    new progress

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,8 +17,8 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'zimaradius.net',
-        'www.zimaradius.net',
+        'zispbilling.cloud',
+        'www.zispbilling.cloud',
         'localhost',
     ],
 
